54b68ac377f3a07d314b17b490b51180ce5b170f
Ali Ateş <techxates@gmail.com>
2023-02-14 14:12:38 +0300

    styling has been made component

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,50 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, Text, TextInput, View} from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 import Card from './Components/Card';
 
 function App() {
   return (
-    <SafeAreaView style={{backgroundColor: 'black', height: '100%'}}>
-      <View
-        style={{
-          margin: 10,
-          paddingVertical: 10,
-        }}>
-        <Text
-          style={{
-            fontSize: 32,
-            fontWeight: '900',
-            color: 'white',
-            textAlign: 'center',
-          }}>
-          Patika Store
-        </Text>
+    <SafeAreaView style={style.container}>
+      <View style={style.logo}>
+        <Text style={style.logo_inner}>Patika Store</Text>
       </View>
       <ScrollView>
-        <TextInput
-          style={{
-            paddingHorizontal: 20,
-            paddingVertical: 10,
-            fontSize: 16,
-            marginHorizontal: 10,
-            borderRadius: 10,
-            backgroundColor: '#252525',
-            marginBottom: 10,
-            fontWeight: '600',
-            color: 'white',
-          }}
-          placeholder="Search"
-        />
+        <TextInput style={style.input} placeholder="Search" />
         <Card />
       </ScrollView>
     </SafeAreaView>
   );
 }
+const style = StyleSheet.create({
+  container: {backgroundColor: 'black', height: '100%'},
+  logo: {
+    margin: 10,
+    paddingVertical: 10,
+  },
+  logo_inner: {
+    fontSize: 32,
+    fontWeight: '900',
+    color: 'white',
+    textAlign: 'center',
+  },
+  input: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    fontSize: 16,
+    marginHorizontal: 10,
+    borderRadius: 10,
+    backgroundColor: '#252525',
+    marginBottom: 10,
+    fontWeight: '600',
+    color: 'white',
+  },
+});
 
 export default App;
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,66 +1,26 @@
 import React from 'react';
-import {Image, Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
 import Stores from './CardJson';
 function Card() {
   return (
-    <View
-      style={{
-        flexDirection: 'row',
-        flexWrap: 'wrap',
-        marginHorizontal: 5,
-      }}>
+    <View style={style.container}>
       {Stores.map((store, key) => {
         return (
-          <View
-            key={key}
-            style={{
-              backgroundColor: '#252525',
-              paddingTop: 10,
-              borderRadius: 10,
-              margin: 5,
-              height: 350,
-              width: '47%',
-            }}>
-            <View style={{borderRadius: 10, flex: 1}}>
+          <View key={key} style={style.container_inner}>
+            <View style={style.container_inner_inner}>
               <Image
-                style={{
-                  borderRadius: 10,
-                  flex: 3.5,
-                  resizeMode: 'cover',
-                  marginHorizontal: 10,
-                }}
+                style={style.img}
                 source={{
                   uri: `${store.imgURL}`,
                 }}
               />
               <View style={{flex: 2, marginHorizontal: 10}}>
-                <Text
-                  numberOfLines={2}
-                  style={{
-                    fontSize: 18,
-                    fontWeight: '500',
-                  }}>
+                <Text numberOfLines={2} style={style.title}>
                   {store.title}
                 </Text>
-                <Text
-                  style={{
-                    color: '#08f26e',
-                    fontSize: 20,
-                    fontWeight: '500',
-                    marginVertical: 5,
-                  }}>
-                  {store.price}
-                </Text>
+                <Text style={style.price}>{store.price}</Text>
                 {store.inStock ? (
-                  <Text
-                    style={{
-                      color: '#ff4444',
-                      fontSize: 14,
-                      fontWeight: '500',
-                      marginTop: 5,
-                    }}>
-                    Stokta Yok
-                  </Text>
+                  <Text style={style.inStock}>Stokta Yok</Text>
                 ) : null}
               </View>
             </View>
@@ -70,4 +30,28 @@ function Card() {
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginHorizontal: 5,
+  },
+  container_inner: {
+    backgroundColor: '#252525',
+    paddingTop: 10,
+    borderRadius: 10,
+    margin: 5,
+    height: 350,
+    width: '47%',
+  },
+  container_inner_inner: {borderRadius: 10, flex: 1},
+  img: {borderRadius: 10, flex: 3.5, resizeMode: 'cover', marginHorizontal: 10},
+  title: {
+    fontSize: 18,
+    fontWeight: '500',
+  },
+  price: {color: '#08f26e', fontSize: 20, fontWeight: '500', marginVertical: 5},
+  inStock: {color: '#ff4444', fontSize: 14, fontWeight: '500', marginTop: 5},
+});
 export default Card;
